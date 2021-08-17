b66099993506b24d31a7be662dce8a448352079a
abdoul <abd.kartish@gmail.com>
2021-08-17 12:38:35 +0200

    h1 added

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -4,6 +4,7 @@ import './../css/main.css'
 export default function Main() {
     return (
         <div className='main'>
+            <h1>this site still under development</h1>
             
         </div>
     )
