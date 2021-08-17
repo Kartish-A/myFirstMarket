8a7ae1aa9ddc5556fa356f001304ebbeed6f1fde
abdoul <abd.kartish@gmail.com>
2021-08-17 11:04:07 +0200

    Updates

---
+++ precache-manifest.350122e5ecc4d51e9cbd71ef358a35b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7e1ff80bc39eb82c81f1e368ea3ac3",
+    "url": "/myFirstMarket/index.html"
+  },
+  {
+    "revision": "d0ae5e938ae4e23786ef",
+    "url": "/myFirstMarket/static/css/2.9a766daa.chunk.css"
+  },
+  {
+    "revision": "cbbba9ad8adaec2a2d5f",
+    "url": "/myFirstMarket/static/css/main.8d6715ba.chunk.css"
+  },
+  {
+    "revision": "d0ae5e938ae4e23786ef",
+    "url": "/myFirstMarket/static/js/2.c2a60087.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/myFirstMarket/static/js/2.c2a60087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbbba9ad8adaec2a2d5f",
+    "url": "/myFirstMarket/static/js/main.777a96b2.chunk.js"
+  },
+  {
+    "revision": "afd8eb7a3f207a2c2b62",
+    "url": "/myFirstMarket/static/js/runtime-main.c8fd8a16.js"
+  }
+]);
